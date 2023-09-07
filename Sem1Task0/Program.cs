115ef50b5cb630138b8c1766ee98e2b725657493
Golikov-Andrey <newsc2@yandex.com>
2023-09-07 13:18:35 +0300

    my first commit

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string? inputLine = Console.ReadLine();
+if (inputLine != null && inputLine != "")
+{
+    Console.WriteLine(inputLine);
+    int inputNum = int.Parse(inputLine);
+    Console.WriteLine(inputNum * inputNum);
+}
